f7a4c5f2ced2785707eaa9d568960cfd9be2ae3f
ekulbyrnes <luke.e.byrnes@gmail.com>
2022-11-21 22:29:46 +1100

    create models and register in admin, views

--- django/float/admin.py
+++ django/float/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Role, Place, Operator, Message
 
 # Register your models here.
+admin.site.register(Role)
+admin.site.register(Place)
+admin.site.register(Operator)
+admin.site.register(Message)
