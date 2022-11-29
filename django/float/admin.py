986dca18cef0d7d1f6a85e068706c7faa5cb901f
ekulbyrnes <luke.e.byrnes@gmail.com>
2022-11-29 23:49:55 +1100

    create emergency patient and message model

--- django/float/admin.py
+++ django/float/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Role, Place, Operator, Message
+from .models import Role, Place, Operator, Message, EmergencyPatient, EmergencyMessage
 
 # Register your models here.
 admin.site.register(Role)
 admin.site.register(Place)
 admin.site.register(Operator)
 admin.site.register(Message)
+admin.site.register(EmergencyPatient)
+admin.site.register(EmergencyMessage)
