b8661e1124ae24c334b20dffc2e77fc1c7c3b8a2
ekulbyrnes <luke.e.byrnes@gmail.com>
2022-12-02 22:24:29 +1100

    add django-simple-history app, migrate

--- django/float/admin.py
+++ django/float/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from .models import Role, Place, Operator, Message, EmergencyPatient, EmergencyMessage
+from simple_history.admin import SimpleHistoryAdmin
 
 # Register your models here.
-admin.site.register(Role)
-admin.site.register(Place)
-admin.site.register(Operator)
-admin.site.register(Message)
-admin.site.register(EmergencyPatient)
-admin.site.register(EmergencyMessage)
+
+admin.site.register(Role, SimpleHistoryAdmin)
+admin.site.register(Place, SimpleHistoryAdmin)
+admin.site.register(Operator, SimpleHistoryAdmin)
+admin.site.register(Message, SimpleHistoryAdmin)
+admin.site.register(EmergencyPatient, SimpleHistoryAdmin)
+admin.site.register(EmergencyMessage, SimpleHistoryAdmin)
