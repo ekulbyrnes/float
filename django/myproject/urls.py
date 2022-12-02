55e893eb0280ddd43b6080261c11a911b752fe56
ekulbyrnes <luke.e.byrnes@gmail.com>
2022-12-03 04:29:51 +1100

    update URLs to customize Django Admin headings

--- django/myproject/urls.py
+++ django/myproject/urls.py
@@ -52,3 +52,7 @@ urlpatterns = [
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "Float: the Log on a Stream"
+admin.site.site_title = "Float Admin Portal"
+admin.site.index_title = "Welcome to the Float Portal"
