586f6f97959ec576ef6ec876fbc4f5e6481dd209
Matthew Cengia <mattcen@mattcen.com>
2022-12-03 19:04:12 +1100

    Remove Security model; merged with Operator

--- django/float/admin.py
+++ django/float/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Role, Place, Operator, Security, Message, IncidentPatient, Incident #, IncidentMessage,
+from .models import Role, Place, Operator, Message, IncidentPatient, Incident #, IncidentMessage,
 from simple_history.admin import SimpleHistoryAdmin
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
@@ -32,10 +32,6 @@ class RoleAdmin(SimpleHistoryAdmin):
 
 class PlaceAdmin(SimpleHistoryAdmin):
     list_display = ('place',)
-
-class SecurityAdmin(SimpleHistoryAdmin):
-    list_display = ('name', 'callsign', 'base', 'last_updated_timestamp',)
-    search_fields = ['name',]
 
 class OperatorAdmin(SimpleHistoryAdmin):
     list_display = ('name', 'callsign', 'base', 'role', 'phone', 'email', 'command_weighting', 'last_updated_timestamp',)
@@ -70,7 +66,6 @@ class IncidentAdmin(SimpleHistoryAdmin):
 
 admin.site.register(Role, RoleAdmin)
 admin.site.register(Place, PlaceAdmin)
-admin.site.register(Security, SecurityAdmin)
 admin.site.register(Operator, OperatorAdmin)
 admin.site.register(Message, MessageAdmin)
 admin.site.register(IncidentPatient, IncidentPatientAdmin)
