d077ee885f464a120e934833f9e5c6d9e12d209c
Matthew Cengia <mattcen@mattcen.com>
2022-12-03 11:34:32 +1100

    Add CSV export to admin interface

--- django/float/admin.py
+++ django/float/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
 from .models import Role, Place, Operator, Message, IncidentPatient, Incident #, IncidentMessage,
 from simple_history.admin import SimpleHistoryAdmin
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse
+import csv
+
+@admin.action(description='Download selected as csv')
+def download_csv(modeladmin, request, queryset):
+    if not request.user.is_staff:
+        raise PermissionDenied
+    opts = queryset.model._meta
+    model = queryset.model
+    response = HttpResponse(content_type='text/csv')
+    # force download.
+    response['Content-Disposition'] = f'attachment;filename={model._meta.model_name}.csv'
+    # the csv writer
+    writer = csv.writer(response)
+    field_names = [field.name for field in opts.fields]
+    # Write a first row with header information
+    writer.writerow(field_names)
+    # Write data rows
+    for obj in queryset:
+        writer.writerow([getattr(obj, field) for field in field_names])
+    return response
+admin.site.add_action(download_csv)
 
 # Create your admin models here.
 
