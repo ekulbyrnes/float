168318d31579c882f0fab331959cc5241a5d0378
Luke Byrnes <luke.e.byrnes@gmail.com>
2025-04-28 19:38:55 +1000

    update message info to change `callsign` to `place` to suit text-entry of ScoutHike SP locations

--- django/float/models.py
+++ django/float/models.py
@@ -49,7 +49,7 @@ class Operator(models.Model):
         help_text='Provide the order in which you wish to have this operator appear in the Message dropdown.')
 
     def __str__(self):
-        return f'{self.callsign} - {self.name} ({self.base})' # returns the Operator's name, role, and assigned location
+        return f'{self.base} - {self.name} ({self.callsign})' # returns the Operator's name, role, and assigned location
         # this helps reconcile the reported location of the Operator with their assigned location and the reported
         # location to assess if further support is required in the field.
 
